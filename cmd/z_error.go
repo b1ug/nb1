b8f1d4e730bdafd65b488f13c5e1d68f0a2113c5
Hyori 효리 <hyorigo@icloud.com>
2023-10-16 22:38:49 +0800

    set color

--- cmd/z_error.go
+++ cmd/z_error.go
@@ -6,4 +6,5 @@ import "errors"
 var (
 	errNotImplemented    = errors.New("not implemented")
 	errConfigKeyNotFound = errors.New("config key not found")
+	errConfigSubKeyBlank = errors.New("config sub-key is blank")
 )
--- util/string.go
+++ util/string.go
@@ -6,3 +6,14 @@ import "strings"
 func NormalizeQuery(raw ...string) string {
 	return strings.TrimSpace(strings.ToLower(strings.Join(raw, " ")))
 }
+
+// SplitConfigKey splits the given config key into two parts: the first part is the top-level key, the second part is the rest.
+func SplitConfigKey(key string) (string, string) {
+	ks := strings.SplitN(strings.ToLower(strings.TrimSpace(key)), ".", 2)
+	k1 := ks[0]
+	var kr string
+	if len(ks) >= 2 {
+		kr = ks[1]
+	}
+	return k1, kr
+}

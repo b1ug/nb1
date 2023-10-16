466393117687322768cc6afc81365f973444cfda
Hyori 효리 <hyorigo@icloud.com>
2023-10-17 00:35:30 +0800

    Use config

--- parser/color.go
+++ parser/color.go
@@ -4,6 +4,7 @@ import (
 	"image/color"
 
 	b1 "github.com/b1ug/blink1-go"
+	"github.com/b1ug/nb1/config"
 )
 
 func ParseColor(raw string) (color.Color, error) {
@@ -17,8 +18,11 @@ func ParseColor(raw string) (color.Color, error) {
 		return b1.RandomColor(), nil
 	}
 
-	// 2. match config
-	// TODO:
+	// 2. match config: use color map as a shortcut
+	cm := config.GetColorMap()
+	if c, ok := cm[raw]; ok {
+		raw = c
+	}
 
 	// 3. for predefined colors, hex, rgb, hsv/hsl hacks
 	st, err := b1.ParseStateQuery("led:0, fade:0, color:" + raw)

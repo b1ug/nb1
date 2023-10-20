e5d59efc3cee520f1377c0f3c5b3b4d8eeb7a281
Hyori 효리 <hyorigo@icloud.com>
2023-10-20 21:03:14 +0800

    Convert TODO

--- cmd/convert.go
+++ cmd/convert.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"errors"
-
 	"github.com/spf13/cobra"
 )
 
@@ -19,11 +18,6 @@ var convertCmd = &cobra.Command{
 		  - Pattern JSON (e.g. '{"repeat":1,"seq":"#FF0000L0T1500;#FF0000L0T3500"...}')
 		  - Starlark Script (e.g. 'play(red, blue, green)')
 	`),
-	Args: cobra.NoArgs,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		//
-		return errors.New("not implemented")
-	},
 }
 
 var (
@@ -40,4 +34,70 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command is called directly, e.g.:
 	// convertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Subcommands
+	convertCmd.AddCommand(convertText2JSONCmd)
+	convertCmd.AddCommand(convertJSON2TextCmd)
+	convertCmd.AddCommand(convertText2ScriptCmd)
+	convertCmd.AddCommand(convertJSON2ScriptCmd)
+}
+
+// convertText2JSONCmd represents the text2json command
+var convertText2JSONCmd = &cobra.Command{
+	Use:     "text2json",
+	Aliases: []string{"textjson", "t2j", "tj"},
+	Short:   "Convert play.txt to pattern.json",
+	Long: hdoc(`
+		Convert a Play Text file to a Pattern JSON file.
+	`),
+	Args: cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		//
+		return errors.New("not implemented")
+	},
+}
+
+// convertJSON2TextCmd represents the json2text command
+var convertJSON2TextCmd = &cobra.Command{
+	Use:     "json2text",
+	Aliases: []string{"json2txt", "j2t", "jt"},
+	Short:   "Convert pattern.json to play.txt",
+	Long: hdoc(`
+		Convert a Pattern JSON file to a Play Text file.
+	`),
+	Args: cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		//
+		return errors.New("not implemented")
+	},
+}
+
+// convertText2ScriptCmd represents the text2script command
+var convertText2ScriptCmd = &cobra.Command{
+	Use:     "text2script",
+	Aliases: []string{"txt2script", "t2s", "ts"},
+	Short:   "Convert play.txt to script.star",
+	Long: hdoc(`
+		Convert a Play Text file to a Starlark Script.
+	`),
+	Args: cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		//
+		return errors.New("not implemented")
+	},
+}
+
+// convertJSON2ScriptCmd represents the json2script command
+var convertJSON2ScriptCmd = &cobra.Command{
+	Use:     "json2script",
+	Aliases: []string{"json2s", "j2s", "js"},
+	Short:   "Convert pattern.json to script.star",
+	Long: hdoc(`
+		Convert a Pattern JSON file to a Starlark Script.
+	`),
+	Args: cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		//
+		return errors.New("not implemented")
+	},
 }

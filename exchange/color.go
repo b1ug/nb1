193cb3707418d1e326f05af112ee5af7a357bd15
Hyori 효리 <hyorigo@icloud.com>
2023-11-05 23:02:57 +0800

    reduce dup code with lib helpers

--- exchange/color.go
+++ exchange/color.go
@@ -26,9 +26,9 @@ func ParseColor(raw string) (color.Color, error) {
 	}
 
 	// 3. for predefined colors, hex, rgb, hsv/hsl hacks
-	st, err := b1.ParseStateQuery("led:0, fade:0, color:" + raw)
+	cl, err := b1.ParseColor(raw)
 	if err != nil {
 		return nil, err
 	}
-	return st.Color, nil
+	return cl, nil
 }
